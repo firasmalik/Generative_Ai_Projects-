308c998d799b30062f720d92b2a6b975dcce71fa
Firas Hamid <firasmalik@gmail.com>
2024-07-21 11:26:41 +0000

    update os

--- Product_reviews/main.py
+++ Product_reviews/main.py
@@ -1,5 +1,6 @@
 # Step 1: Import Libraries and Load the Model
 import numpy as np
+import os
 import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence

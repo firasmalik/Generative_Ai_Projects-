974215ab0ea72456c1dec2b75434beae6bb71b9f
Firas Hamid <firasmalik@gmail.com>
2024-07-21 11:17:51 +0000

    main.py update model

--- Product_reviews/main.py
+++ Product_reviews/main.py
@@ -10,7 +10,8 @@ word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load the pre-trained model with ReLU activation
-model = load_model('simple_rnn_imdb.h5')
+#model = load_model('simple_rnn_imdb.h5')
+model = load_model(os.path.join(os.getcwd(), 'simple_rnn_imdb.h5'))
 
 # Step 2: Helper Functions
 # Function to decode reviews
@@ -47,3 +48,4 @@ if st.button('Classify'):
     st.write(f'Prediction Score: {prediction[0][0]}')
 else:
     st.write('Please enter a movie review.')
+

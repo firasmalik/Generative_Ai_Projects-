0acb3678a81af01f9a991949b4adadf2e5be721b
Firas Hamid <firasmalik@gmail.com>
2024-07-29 10:46:00 +0000

    env added

--- Ollama_App/app.py
+++ Ollama_App/app.py
@@ -1,1 +1,17 @@
-import streamlit as sr
+import streamlit as st
+
+import os
+from dotenv import load_dotenv
+
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+## Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
